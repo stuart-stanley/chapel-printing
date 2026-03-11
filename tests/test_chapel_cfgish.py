d6d271a280968e39a3611ef5b69ac97512db771c
Stuart Stanley <stuart@glowforge.com>
2026-03-11 16:24:01 -0700

    flake it

--- tests/test_chapel_cfgish.py
+++ tests/test_chapel_cfgish.py
@@ -8,6 +8,7 @@ def _builtin_cfg():
     cp_cfg = load_chapel_config()
     yield cp_cfg
 
+
 def _test_cfgish():
     heredir = Path(__file__).resolve().parent
 
@@ -15,18 +16,22 @@ def _test_cfgish():
     tcfg = load_chapel_config(tcfg_path)
     return tcfg
 
+
 @pytest.fixture
 def _test_cfgish_fx():
     tcfg = _test_cfgish()
     yield tcfg
 
+
 def test_builting_load():
     cp_cfg = load_chapel_config()
     assert cp_cfg is not None
 
+
 def test_load_from_file():
     tcfg = _test_cfgish()
     assert tcfg is not None
+
 
 def test_basic_layout_style(_builtin_cfg):
     assert hasattr(_builtin_cfg, "overall_style_version")
@@ -41,7 +46,7 @@ def test_basic_layout_printer_geometry(_builtin_cfg):
 def test_printer_version(_test_cfgish_fx):
     assert _test_cfgish_fx.overall_style_version == 1
 
-    
+
 def test_printer_geometry(_test_cfgish_fx):
     assert 'M200V2' in _test_cfgish_fx.printer_geometry
     m2 = _test_cfgish_fx.printer_geometry['M200V2']
